72e329c08ef5ea6bea0539c4c1b44d3dd9635bfe
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-12 15:56:14 +0300

    Add files via upload

---
+++ guiTask_11/guiTask_11/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleTask_11
+{
+    class Class1
+    {
+        int[] m_IntArray;
+        int n;
+        public Class1(int n)
+        {
+            this.n = n;
+            m_IntArray = new int[n];
+        }
+        public Class1():this(0)
+        {
+
+        }
+        public void Input(string str)
+        {
+            string[] str2 = str.Split(' ');
+            if(str2.Length>m_IntArray.Length || str2.Length < m_IntArray.Length)
+            {
+                throw new Exception("Некорректное количество введенных элементов");
+            }
+            else
+            {
+                for (int i=0; i<m_IntArray.Length; i++)
+                {
+                    m_IntArray[i] = int.Parse(str2[i]);
+                }
+            }
+        }
+        public string Show()
+        {
+            string str = String.Empty;
+            foreach(int a in m_IntArray)
+            {
+                str += a + " ";
+            }
+            return str;
+        }
+        public void Sort()
+        {
+            for (int i=0; i<m_IntArray.Length; i++)
+            {
+                for (int j =0; j<m_IntArray.Length-1; j++)
+                {
+                    if (m_IntArray[j]>m_IntArray[j+1])
+                    {
+                        int buf = m_IntArray[j+1];
+                        m_IntArray[j + 1] = m_IntArray[j];
+                        m_IntArray[j] = buf;
+                    }
+                }
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                return m_IntArray.Length;
+            }
+        }
+        public int Skal
+        {
+            set
+            {
+                for (int i = 0; i < m_IntArray.Length; i++)
+                {
+                    m_IntArray[i] *= value;
+                }
+            }
+        }
+    }
+}
---
+++ guiTask_11/guiTask_11/Form1.cs
@@ -0,0 +1,38 @@
+﻿using consoleTask_11;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guiTask_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Class1 o = new Class1(int.Parse(tbN.Text));
+                o.Input(tbArr.Text);
+                o.Sort();
+                labSortArr.Text = "Сортированный массив: " + o.Show();
+                o.Skal = int.Parse(tbSkal.Text);
+                labRes.Text = "Массив: " + o.Show();
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Ошибка");
+            }
+        }
+    }
+}
